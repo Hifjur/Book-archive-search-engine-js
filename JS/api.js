d90de065027771ccf37c593a663c1729f8d63110
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-09-02 19:12:42 +0600

    spinner added

--- JS/api.js
+++ JS/api.js
@@ -1,11 +1,13 @@
 // taking input from the input field
-const searchInput = () =>{
-    const previousResult= document.getElementById('search-result');
+const searchInput = () => {
+    const previousResult = document.getElementById('search-result');
     previousResult.textContent = '';
+    document.getElementById('spinner').style.display="block"
     const text = document.getElementById('search-text');
     bookArchiveConnetor(text.value);
     text.value = '';
-    document.getElementById('search-count').style.display='none';
+
+    document.getElementById('search-count').style.display = 'none';
 }
 
 //procecssing the input to fetch result
@@ -13,78 +15,79 @@ const bookArchiveConnetor = bookName => {
     const bookUrl = `https://openlibrary.org/search.json?q=${bookName}`;
     console.log(bookUrl);
     fetch(bookUrl)
-    .then(res => res.json())
-    .then(data => totalFindingResults(data));
-   
+        .then(res => res.json())
+        .then(data => totalFindingResults(data));
+
 }
 
 //get eh data and shows search reasult
-const totalFindingResults= data =>{
+const totalFindingResults = data => {
     console.log(data);
-    const resutlCount= document.getElementById('search-count-value');
+    const resutlCount = document.getElementById('search-count-value');
     const counter = data.numFound;
-    if(counter === 0){
-        resutlCount.innerHTML=`
+    if (counter === 0) {
+        resutlCount.innerHTML = `
         No result found
     `;
     }
-    else{
-    resutlCount.innerHTML=`
+    else {
+        resutlCount.innerHTML = `
         About ${counter} results found..
     `;
     }
-    document.getElementById('search-count').style.display='block';
+    document.getElementById('search-count').style.display = 'block';
 
     diplayResult(data.docs);
 };
 
 //getting author and getting rid of undefined values
 const getAuthor = book => {
-    if (book.author_name === undefined){
+    if (book.author_name === undefined) {
         return 'Author Not Found!';
     }
-    else{
+    else {
         return book.author_name;
     };
 }
 //getting publisher and getting rid of undefined values
 const getPublisher = book => {
-    if (typeof(book.publisher) === 'object'){
+    if (typeof (book.publisher) === 'object') {
         return book.publisher[0];
     }
-    else if (book.publisher === undefined){
+    else if (book.publisher === undefined) {
         return 'Not found!';
     }
-    else{
+    else {
         return book.publisher;
     };
 }
 //getting publishing year and getting rid of undefined values
 const getPublishYear = book => {
-    if (typeof(book.publish_year) === 'object'){
+    if (typeof (book.publish_year) === 'object') {
         const yearArray = book.publish_year;
         return Math.min(...yearArray);
     }
-    else if (book.publish_year === undefined){
+    else if (book.publish_year === undefined) {
         return 'Not found!';
     }
-    else{
+    else {
         return book.publish_year;
     };
 }
 //getting cover photo    
 const getCover = book => {
-    const coverPhoto= `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-    if (book.cover_i === undefined){
+    const coverPhoto = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    if (book.cover_i === undefined) {
         return `images/no_preview.png`;
     }
-    else{
+    else {
         return coverPhoto;
     };
-}    
+}
 //search result
 const diplayResult = books => {
     console.log(books);
+    document.getElementById('spinner').style.display="none"
     const result = document.getElementById('search-result');
     books.forEach(book => {
         const author = getAuthor(book);
@@ -100,8 +103,8 @@ const diplayResult = books => {
                 <h5 class="card-title">${book.title}</h5>
                 <p class="card-text">by ${author} <br>
                 Publisher: ${publisher} <br>
-                Publish Year: ${publishYear}
-                .</p>
+                Original Publishing Year: ${publishYear}
+                </p>
             </div>
         </div>
         `;
