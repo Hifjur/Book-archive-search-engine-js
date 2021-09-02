2a953f896aef97f330de1db0b9be8ca1ffe8fe8a
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-09-02 08:02:39 +0600

    cover added

--- JS/api.js
+++ JS/api.js
@@ -7,26 +7,33 @@ const searchInput = () =>{
 
 //procecssing the input to fetch result
 const bookArchiveConnetor = bookName => {
-    const url = `https://openlibrary.org/search.json?q=${bookName}`;
-    console.log(url);
-    fetch(url)
+    const bookUrl = `https://openlibrary.org/search.json?q=${bookName}`;
+    console.log(bookUrl);
+    fetch(bookUrl)
     .then(res => res.json())
     .then(data => diplayResult(data.docs));
+
+    //const picture.
     
 }
+
 
 const diplayResult = books => {
     const result = document.getElementById('search-result');
     books.forEach(book => {
-        console.log(book);
+        const coverPhoto= `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        
+        console.log(coverPhoto);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 bg-card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${coverPhoto}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${book.title}</h5>
+                <p class="card-text">by ${book.author_name} <br>
+                Publisher: ${books.publisher}
+                .</p>
             </div>
         </div>
         `;
